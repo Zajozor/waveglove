6d7ab5e541104b242890503dfdcc2941b005abc4
zajozor <github@zajo.tk>
2020-05-13 09:56:39 +0200

    Add padding to CNN

--- models/m4_cnn.py
+++ models/m4_cnn.py
@@ -9,26 +9,29 @@ class CNNModel(CommonModel):
     def __init__(self, hparams, xst, yst, xsv, ysv):
         super().__init__(hparams, xst, yst, xsv, ysv)
 
+        self.zp1 = torch.nn.ZeroPad2d((0, 1, 0, hparams['kernel_width1'] - 1))
         self.c1 = torch.nn.Conv2d(1, hparams['filters1'], kernel_size=(hparams['kernel_width1'], 2))
         self.mp1 = torch.nn.MaxPool2d(kernel_size=(2, 1))
 
+        self.zp2 = torch.nn.ZeroPad2d((0, 0, 0, hparams['kernel_width2'] - 1))
         self.c2 = torch.nn.Conv2d(hparams['filters1'], hparams['filters2'], kernel_size=(hparams['kernel_width2'], 1))
+        self.mp2 = torch.nn.MaxPool2d(kernel_size=(2, 1))
 
+        self.zp3 = torch.nn.ZeroPad2d((0, 0, 0, hparams['kernel_width3'] - 1))
         self.c3 = torch.nn.Conv2d(hparams['filters2'], hparams['filters3'], kernel_size=(hparams['kernel_width3'], 1))
         self.mp3 = torch.nn.MaxPool2d(kernel_size=(2, 1))
 
-        width_after1 = (hparams['temporal_length'] - hparams['kernel_width1'] + 1) // 2
-        width_after2 = width_after1 - hparams['kernel_width2'] + 1
-        width_after3 = (width_after2 - hparams['kernel_width3'] + 1) // 2
+        width_after3 = hparams['temporal_length'] // 8
 
-        self.final = torch.nn.Linear(hparams['filters3'] * width_after3 * (hparams['channels'] - 1),
+        self.final = torch.nn.Linear(hparams['filters3'] * width_after3 * hparams['channels'],
                                      hparams['class_count'])
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = x.view((x.shape[0], 1, x.shape[1], x.shape[2]))
-        x = self.mp1(torch.relu(self.c1(x)))
-        x = torch.relu(self.c2(x))
-        x = self.mp3(torch.relu(self.c3(x)))
+
+        x = self.mp1(torch.relu(self.c1(self.zp1(x))))
+        x = self.mp2(torch.relu(self.c2(self.zp2(x))))
+        x = self.mp3(torch.relu(self.c3(self.zp3(x))))
 
         return self.final(x.flatten(start_dim=1))
 
